76f39847c9b1d6c741e69a2cc34e6cb029bdcecb
shimura.yuki <yukis4san@gmail.com>
2017-10-27 05:18:44 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -56,7 +56,7 @@ class MessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
   
 end
